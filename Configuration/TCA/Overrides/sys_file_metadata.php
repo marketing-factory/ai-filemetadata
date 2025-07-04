6bc6c5059ca15b3fdf3707fa2780e6fb29c3262d
Ingo Schmitt <is@marketing-factory.de>
2025-07-04 11:49:57 +0200

    implement feature

--- Configuration/TCA/Overrides/sys_file_metadata.php
+++ Configuration/TCA/Overrides/sys_file_metadata.php
@@ -1,3 +1,21 @@
 <?php
 
 $GLOBALS['TCA']['sys_file_metadata']['columns']['alternative']['config']['renderType'] = 'aiGeneratedAltText';
+
+$additionalColumns = [
+    'alttext_generation_date' => [
+        'exclude' => 1,
+        'label' => 'LLL:EXT:ai-filemetadata/Resources/Private/Language/locallang_ai_filemetadata.xlf:sys_file_metadata.alttext_generation_date',
+        'config' => [
+            'type' => 'input',
+            'renderType' => 'inputDateTime',
+            'eval' => 'datetime,int',
+            'readOnly' => true,
+        ],
+        // Optionally only show if set:
+        // 'displayCond' => 'FIELD:alttext_generation_date:>:0',
+    ],
+];
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('sys_file_metadata', $additionalColumns);
+// Do not add to any showitem, so it's not editable in backend forms
