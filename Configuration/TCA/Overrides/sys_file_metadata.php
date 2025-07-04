1abdb16ba04c9657526faf9d4696cd5ec5b29cf9
Ingo Schmitt <is@marketing-factory.de>
2025-07-04 11:58:12 +0200

    show field in backend

--- Configuration/TCA/Overrides/sys_file_metadata.php
+++ Configuration/TCA/Overrides/sys_file_metadata.php
@@ -13,9 +13,17 @@ $additionalColumns = [
             'readOnly' => true,
         ],
         // Optionally only show if set:
-        // 'displayCond' => 'FIELD:alttext_generation_date:>:0',
+        'displayCond' => 'FIELD:alttext_generation_date:>:0',
     ],
 ];
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('sys_file_metadata', $additionalColumns);
 // Do not add to any showitem, so it's not editable in backend forms
+
+// Add alttext_generation_date to showitem after alternative
+$GLOBALS['TCA']['sys_file_metadata']['types']['1']['showitem'] =
+    str_replace(
+        'alternative,',
+        'alternative,alttext_generation_date,',
+        $GLOBALS['TCA']['sys_file_metadata']['types']['1']['showitem']
+    );
