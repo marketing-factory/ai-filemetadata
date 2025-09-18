be79328b6cf7ddbe3b21d6eb9dae8013fa599f11
Stefan Froemken <sfroemken@jweiland.net>
2025-09-18 10:31:07 +0200

    Update comment for AI AJAX route

--- Configuration/Backend/AjaxRoutes.php
+++ Configuration/Backend/AjaxRoutes.php
@@ -3,10 +3,9 @@
 use Mfd\Ai\FileMetadata\Backend\Controller;
 
 return [
-    // Validate slug input
-                    'record_ai_generated_alt_text' => [
+    // AJAX route for AI-generated alt text suggestions
+    'record_ai_generated_alt_text' => [
         'path' => '/record/ai-generated-alt-text',
         'target' => Controller\AiGeneratedAltTextAjaxController::class . '::suggestAction',
-                    ],
-
+    ],
 ];
