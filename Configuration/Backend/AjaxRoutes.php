8994f511047a7638c55a4b46487cfb29527344e0
Stefan Froemken <sfroemken@jweiland.net>
2025-09-18 10:27:50 +0200

    Use proper indentation in AjaxRoutes.php

--- Configuration/Backend/AjaxRoutes.php
+++ Configuration/Backend/AjaxRoutes.php
@@ -4,9 +4,8 @@ use Mfd\Ai\FileMetadata\Backend\Controller;
 
 return [
     // Validate slug input
-                    'record_ai_generated_alt_text' => [
+    'record_ai_generated_alt_text' => [
         'path' => '/record/ai-generated-alt-text',
         'target' => Controller\AiGeneratedAltTextAjaxController::class . '::suggestAction',
-                    ],
-
+    ],
 ];
